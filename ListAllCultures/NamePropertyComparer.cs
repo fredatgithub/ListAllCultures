fe117e06f897205254c35b49a527a4cdd208574e
fred <domaine75@gmail.com>
2022-07-31 13:13:34 +0200

    Ajoutez des fichiers projet.

---
+++ ListAllCultures/NamePropertyComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace ListAllCultures
+{
+  public class NamePropertyComparer<T> : IComparer<T>
+  {
+    public int Compare(T x, T y)
+    {
+      if (x == null)
+        if (y == null)
+          return 0;
+        else
+          return -1;
+
+      PropertyInfo pX = x.GetType().GetProperty("Name");
+      PropertyInfo pY = y.GetType().GetProperty("Name");
+      return string.Compare((string)pX.GetValue(x, null), (string)pY.GetValue(y, null));
+    }
+  }
+
+}
---
+++ ListAllCultures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ListAllCultures
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // Display the header.
+      Console.WriteLine("{0,-53}{1}", "CULTURE", "SPECIFIC CULTURE");
+
+      // Get each neutral culture in the .NET Framework.
+      CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.NeutralCultures);
+      // Sort the returned array by name.
+      Array.Sort<CultureInfo>(cultures, new NamePropertyComparer<CultureInfo>());
+
+      // Determine the specific culture associated with each neutral culture.
+      foreach (var culture in cultures)
+      {
+        Console.Write("{0,-12} {1,-40}", culture.Name, culture.EnglishName);
+        try
+        {
+          Console.WriteLine("{0}", CultureInfo.CreateSpecificCulture(culture.Name).Name);
+        }
+        catch (ArgumentException)
+        {
+          Console.WriteLine("(no associated specific culture)");
+        }
+      }
+
+      Console.WriteLine("press any key to exit:");
+      Console.ReadKey();
+
+    }
+  }
+}
